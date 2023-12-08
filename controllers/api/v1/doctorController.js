949e2eebf8117d570da34f57080ed6929946074e
dhanashrirandive06 <dhanashrirandive06.com>
2023-12-08 13:04:29 +0530

    version 3

--- controllers/api/v1/doctorController.js
+++ controllers/api/v1/doctorController.js
@@ -22,7 +22,7 @@ module.exports.register = async (req, res, next) => {
 
       return res.status(200).json({
         message: "Doctor Register Successfully",
-        doctor: user,
+        doctor: newDoctor,
       });
     }
     return res.status(200).json({
